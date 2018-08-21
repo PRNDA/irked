671f5554ca6dccd6c7a576a48b66a686b09fa5f0
Oded Arbel <oded@geek.co.il>
2018-08-21 13:09:39 +0300

    WIP

--- src/main/java/tech/greenfield/vertx/irked/annotations/OnFail.java
+++ src/main/java/tech/greenfield/vertx/irked/annotations/OnFail.java
@@ -10,4 +10,6 @@ import java.lang.annotation.Target;
 @Target({FIELD, METHOD})
 public @interface OnFail {
 
+	Class<?>[] exceptions() default {};
+
 }
--- src/test/java/tech/greenfield/vertx/irked/TestFailController.java
+++ src/test/java/tech/greenfield/vertx/irked/TestFailController.java
@@ -10,32 +10,44 @@ import tech.greenfield.vertx.irked.annotations.Endpoint;
 import tech.greenfield.vertx.irked.annotations.Get;
 import tech.greenfield.vertx.irked.annotations.OnFail;
 import tech.greenfield.vertx.irked.base.TestBase;
+import tech.greenfield.vertx.irked.status.BadRequest;
 
 public class TestFailController extends TestBase {
 
-	public class TestController extends Controller {
-		@OnFail
-		@Endpoint("/*")
-		WebHandler failureHandler = r -> {
-			r.sendJSON(new JsonObject().put("success", false));
-		};
-		
+	public class FailController extends Controller {
 		@Get("/")
 		WebHandler index = r -> {
 			throw new RuntimeException();
+		};
+		
+		@Get("/arg")
+		WebHandler witharg = r -> {
+			throw new IllegalArgumentException();
+		};
+		
+		@OnFail(exceptions = {IllegalArgumentException.class})
+		@Endpoint("/*")
+		WebHandler invalidArgHandler = r -> {
+			r.fail(new BadRequest(r.failure()));
+		};
+		
+		@OnFail
+		@Endpoint("/*")
+		WebHandler failureHandler = r -> {
+			r.sendJSON(new JsonObject().put("success", false), HttpError.toHttpError(r));
 		};
 	}
 
 	@Before
 	public void deployServer(TestContext context) {
-		deployController(new TestController(), context.asyncAssertSuccess());
+		deployController(new FailController(), context.asyncAssertSuccess());
 	}
 
 	@Test
 	public void testFail(TestContext context) {
 		Async async = context.async();
 		getClient().get(port, "localhost", "/").exceptionHandler(t -> context.fail(t)).handler(res -> {
-			context.assertEquals(200, res.statusCode(), "Request failed");
+			context.assertEquals(500, res.statusCode(), "Request failed");
 			res.exceptionHandler(t -> context.fail(t)).bodyHandler(body -> {
 				try {
 					JsonObject o = body.toJsonObject();
@@ -48,4 +60,20 @@ public class TestFailController extends TestBase {
 		}).end();
 	}
 
+	@Test
+	public void testSelectiveFail(TestContext context) {
+		Async async = context.async();
+		getClient().get(port, "localhost", "/arg").exceptionHandler(t -> context.fail(t)).handler(res -> {
+			context.assertEquals(400, res.statusCode(), "Request failed");
+			res.exceptionHandler(t -> context.fail(t)).bodyHandler(body -> {
+				try {
+					JsonObject o = body.toJsonObject();
+					context.assertEquals(Boolean.FALSE, o.getValue("success"));
+				} catch (Exception e) {
+					context.fail(e);
+				}
+			});
+			async.complete();
+		}).end();
+	}
 }
